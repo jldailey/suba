2ea3f078aeb622a3a82f43c2049f88455a689341
Jesse Dailey <jesse.dailey@gmail.com>
2010-11-28 14:29:34 -0500

    cleanup the repository

--- test/run-tests.py
+++ test/run-tests.py
@@ -1,20 +1,22 @@
-from suba import template
+#!/usr/bin/env python3.1
 import os, sys
+sys.path.insert(0,"..")
+from suba import template
 
 test_to_run = None
 if len(sys.argv) > 1:
 	test_to_run = sys.argv[1]
 
-for file in os.listdir("test"):
+for file in os.listdir("."):
 	if file.endswith(".test"):
 		if test_to_run is None or file.startswith(test_to_run):
 			try:
-				output = ''.join(template(filename=file, root="test", stripWhitespace=True, names = ['John','Paul','Ringo']))
+				output = ''.join(template(filename=file, root=".", stripWhitespace=True, names = ['John','Paul','Ringo']))
 			except Exception as e:
 				output = str(e)
 				if test_to_run is not None:
 					raise
-			correct = open(os.path.sep.join(["test",file.replace(".test",".output")]), "r").read()[:-1]
+			correct = open(os.path.sep.join([".",file.replace(".test",".output")]), "r").read()[:-1]
 			if output != correct:
 				print(file,"FAIL:")
 				print("EXPECTED:")
